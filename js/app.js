2225c982bb12b2f8bf7eab4c72609dbf1aa4b70c
Kawandeep Virdee <whichlight@gmail.com>
2016-01-16 16:02:56 -0500

    better for mobile

--- js/app.js
+++ js/app.js
@@ -13,6 +13,9 @@ var growth= growth0;
 var state = "calm";
 var deluge = 0;
 var context;
+var pool = [];
+
+var host = "tumblr";
 
 
 var angle = 0;
@@ -26,16 +29,17 @@ var refresh_rate = 20000;
 
 //2000 1.01  --> 4 drops
 
-var MAX_ON_SCREEN = 25;
+var MAX_ON_SCREEN = 10;
 var num_drops = 0;
 
 function Drop(src){
   this.src = src;
-  this.x = width/2;
-  this.y = height/2;
+  this.x = 0;
+  this.y = 0;
   this.rx = 30;
-  this.image = new Image();
-  this.image.src = this.src;
+  this.ry = 30;
+
+  this.image = src;
   $(this.image).addClass("drop");
   $("#flood").append(this.image);
 }
@@ -44,32 +48,47 @@ Drop.prototype.update = function(){
 
   var $i = $(this.image);
 
-  this.rx *= growth;
-  this.ry = $i.height()
+  if(width>=height){
+    this.rx *= growth;
+    this.image.style.width = this.rx + "px";
+    this.ry = $i.height();
+    this.image.style.height= 'auto';
+    this.image.style.opacity= Math.abs((this.rx-width*3)/(width*3));
 
-  //visual
-  $i.css('left', this.x - this.rx/2);
-  $i.css('top', this.y - this.ry/2);
-  this.image.style.width = this.rx + "px";
-  this.image.style.height= 'auto';
-  this.image.style.opacity= Math.abs((this.rx-width*2)/(width*2));
+    if(this.rx > width*2.5){
+      // console.log('too big, removed');
+      drops.splice(drops.indexOf(this),1);
+      $(this.image).remove();
+    }
 
-  //remove
-  if(this.rx > width*2){
-     // console.log('too big, removed');
-     drops.splice(drops.indexOf(this),1);
-     $(this.image).remove();
+  }else
+  {
+    this.ry *= growth;
+    this.image.style.height= this.ry + "px";
+    this.rx = $i.width()
+      this.image.style.width= 'auto';
+    this.image.style.opacity= Math.abs((this.ry-height*3)/(height*3));
+
+
+    if(this.ry > height*3){
+      // console.log('too big, removed');
+      drops.splice(drops.indexOf(this),1);
+      $(this.image).remove();
+    }
+
   }
+
+
 }
 
 function render() {
   requestAnimationFrame(render);
 
-//logo
+  //logo
 
- $(logo).css('left', width/2 - logo.width/2+"px");
-  $(logo).css('top', height/2 - logo.height/2+ "px");
-   logo.style.webkitTransform = "rotate(-"+angle+"deg)";
+  //$(logo).css('left', width/2 - logo.width/2+"px");
+  //$(logo).css('top', height/2 - logo.height/2+ "px");
+  logo.style.webkitTransform = "rotate(-"+angle+"deg)";
 
 
   if(deluge ==0){
@@ -113,7 +132,7 @@ function render() {
   }
 
   if (state == "calm"){
- //   $('img').remove();
+    //   $('img').remove();
   }
 
   update();
@@ -125,17 +144,18 @@ function render() {
 function update(){
 
   num_drops = $('.drop').length
+  console.log(drop_rate);
 
-  if(num_drops == 0){
-    createDrop();
-  }
+    if(num_drops == 0){
+      createDrop();
+    }
   drops.forEach(function(d){
     d.update();
   });
 }
 
 blacklist = [
-  'www.tumblr.com',
+'www.tumblr.com',
   'dednewb.tumblr.com',
   'kendrickxlamar.tumblr.com',
   'hellyeahrihannafenty.tumblr.com',
@@ -149,15 +169,15 @@ blacklist = [
   // 'furples.tumblr.com',
   // mikehottman.tumblr.com
   // olivegarden.tumblr.com
-]
+  ]
 
-function pickHost() {
-  host = pickRandomProperty(data);
-  while(data[host].length < 15 || $.inArray(host, blacklist) != -1 || countGoodImages(host) < 5) {
-    delete data.host;
+  function pickHost() {
     host = pickRandomProperty(data);
+    while(data[host].length < 15 || $.inArray(host, blacklist) != -1 || countGoodImages(host) < 5) {
+      delete data.host;
+      host = pickRandomProperty(data);
+    }
   }
-}
 
 function countGoodImages(host) {
   var count = 0;
@@ -170,12 +190,12 @@ function countGoodImages(host) {
 }
 
 function pickRandomProperty(obj) {
-    var result;
-    var count = 0;
-    for (var prop in obj)
-        if (Math.random() < 1/++count)
-           result = prop;
-    return result;
+  var result;
+  var count = 0;
+  for (var prop in obj)
+    if (Math.random() < 1/++count)
+      result = prop;
+  return result;
 }
 
 function getRandomElement(arr){
@@ -188,15 +208,13 @@ function getRandomElement(arr){
 }
 
 function createDrop(){
-
   if( num_drops < MAX_ON_SCREEN && state == "deluge"){
-    var val = getRandomElement(data[host]);
-    var src = "media/" + val;
-    var d = new Drop(src)
-      drops.push(d);
-  setTimeout(function(){
-    createDrop();
-  },drop_rate);
+    var val = getRandomElement(pool);
+    var d = new Drop(val);
+    drops.push(d);
+    setTimeout(function(){
+      createDrop();
+    },drop_rate);
   }
 }
 
@@ -204,10 +222,10 @@ function createDrop(){
 
 
 /*
-$("#flood").click(function(){
-  createDrop();
-})
-*/
+   $("#flood").click(function(){
+   createDrop();
+   })
+   */
 
 $("#flood").mouseup(function(){
   deluge = 0;
@@ -215,43 +233,43 @@ $("#flood").mouseup(function(){
 
 $("#flood").mousedown(function(){
   if (deluge == 0) {
-    pickHost();
+    host="tumblr";
   }
   deluge = 1;
 });
 
 var handleStart = function(e){
-    e.preventDefault();
-    deluge = 1;
+  e.preventDefault();
+  deluge = 1;
 }
 
 
 var handleEnd = function(e){
-    e.preventDefault();
-    deluge = 0;
+  e.preventDefault();
+  deluge = 0;
 }
 
 $(document).keyup(function(evt) {
   if (evt.keyCode == 32) {
-  deluge = 0;
+    deluge = 0;
     space = false;
   }
 }).keydown(function(evt) {
 
   if (evt.keyCode == 32) {
-  evt.stopPropagation();
-   evt.preventDefault();
-     if (deluge == 0) {
-    pickHost();
-  }
-  deluge = 1;
+    evt.stopPropagation();
+    evt.preventDefault();
+    if (deluge == 0) {
+      host="tumblr";
+    }
+    deluge = 1;
     space = true;
   }
 });
 
 
 function getData(res) {
-  $.getJSON("http://localhost:8080/data/data.js",function(response) {
+  $.getJSON("./data/data.js",function(response) {
     data = response;
     if(res) {
       res();
@@ -260,11 +278,20 @@ function getData(res) {
   setTimeout(function(){getData()},refresh_rate);
 }
 
+function pdefault(e){
+    e.preventDefault()
+}
+
 function init(){
 
-    var el = document.getElementById("flood");
-    el.addEventListener("touchstart", handleStart, false);
-    el.addEventListener("touchend", handleEnd, false);
+  var el = document.getElementById("flood");
+  el.addEventListener("touchstart", handleStart, false);
+  el.addEventListener("touchend", handleEnd, false);
+  el.addEventListener("touchstart", pdefault, false);
+  el.addEventListener("touchend", pdefault, false);
+  el.addEventListener("touchcancel", pdefault, false);
+  el.addEventListener("touchleave", pdefault, false);
+  el.addEventListener("touchmove", pdefault, false);
 
   logo = new Image();
   logo.src = "media/snake-logo-1.gif";
@@ -304,6 +331,7 @@ function init(){
 
 
 
+
   setInterval(function(){
 
     if(deluge ==1){
@@ -311,7 +339,7 @@ function init(){
         growth+=0.009
       }
 
-      if(num_drops<8){
+      if(num_drops<4){
         drop_rate -=10;
       }else{
         drop_rate +=10;
@@ -336,16 +364,53 @@ function init(){
 
 $(document).ready(function(){
 
-    infoAlert();
+  infoAlert();
+  $("#loading").html("0%");
   checkFeatureSupport();
-    init();
+  var srcs = data.tumblr.map(function(v){return "media/"+v;});
+  preloadImages(srcs, init);
 });
 
+
+var preloadImages = function(srcs, cb) {
+    var loaded = 0;
+
+    if (!preloadImages.cache) {
+        preloadImages.cache = [];
+    }
+    var img;
+    for (var i = 0; i < srcs.length; i++) {
+        img = new Image();
+        img.src = srcs[i];
+        pool.push(img);
+        img.onload = function(){
+            loaded++;
+            $("#loading").html(Math.ceil(loaded/srcs.length*100)+"%");
+            if(loaded == srcs.length){
+                $("#loading").remove();
+                cb();
+            }
+        };
+        preloadImages.cache.push(img);
+
+
+    }
+}
 
 var checkFeatureSupport = function(){
   try{
     window.AudioContext = window.AudioContext||window.webkitAudioContext;
     context = new AudioContext();
+    var iOS = ( navigator.userAgent.match(/(iPad|iPhone|iPod)/g) ? true : false );
+    if (iOS) {
+        window.addEventListener('touchend', function() {
+            var buffer = context.createBuffer(1, 1, 22050);
+            var source = context.createBufferSource();
+            source.buffer = buffer;
+            source.connect(context.destination);
+            source.start(0);
+        }, false);
+    }
   }
   catch (err){
     alert('web audio not supported');
@@ -360,5 +425,5 @@ var checkFeatureSupport = function(){
 }
 
 var infoAlert = function(){
-      alert("Web Warp \nRelax into the sensation and absorb the images as they arrive. Gently, then more rapidly, they will engulf you. Face the cascade, savor the flow.\n\nby Audrey Fox, Cameron Ketcham, Kawandeep Virdee, James Donovan.\nMade for ArtHackDay Deluge.");
+  alert("Web Warp \nRelax into the sensation and absorb the images as they arrive. Gently, then more rapidly, they will engulf you. Face the cascade, savor the flow.\n\nby Audrey Fox, Cameron Ketcham, Kawandeep Virdee, James Donovan.\nMade for ArtHackDay Deluge.");
 }
